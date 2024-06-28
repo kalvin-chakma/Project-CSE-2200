6f4dbc6c77805117ec1f109fee76aeeb9eb45559
kalvin-chakma <[kalvinchakma7@gmail.com]>
2024-06-28 16:59:24 +0600

    fix: router changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import Context from "./utills/Context.jsx"; // Corrected the path to Context.jsx
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Context>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Context>
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
