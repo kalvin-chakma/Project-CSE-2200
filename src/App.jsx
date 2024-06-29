25834f1355ba962dd595d8d4fc2a7d40c78e82b4
Sharadindu Dutta <135245823+Turjo24@users.noreply.github.com>
2024-06-29 16:17:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import BrowserRouter and rename it to Router
+import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
-import Create from "./components/Create"; // Correct the import path for Create component
+import Navbar from "./components/Navbar";
+import Create from "./components/Create";
 import Details from "./components/Details";
 
 export default function App() {
   return (
-    <Router>
-      {" "}
-      {/* Wrap Routes in Router */}
+    <div className="h-screen w-screen flex flex-col">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/create" element={<Create />} />{" "}
-        {/* Corrected path for Create component */}
+        <Route path="/create" element={<Create />} />
         <Route path="/details/:id" element={<Details />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
+
